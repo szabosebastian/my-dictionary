f83fe79ce48540f70df5fa826693a04f494f6ea6
Szabó Sebastián <szabo.sebastian@nextval.hu>
2023-07-25 16:16:37 +0200

    todo

--- src/app/state/workbook/workbook.effects.ts
+++ src/app/state/workbook/workbook.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType, ROOT_EFFECTS_INIT } from '@ngrx/effects';
-import { switchMap, take, tap } from "rxjs";
+import { from, map, switchMap, take, tap } from "rxjs";
 import { initStorage, initWorkbook, setWorkbook } from "./workbook.actions";
 import { WORKBOOK } from "../../core/model/workbook";
 import { StorageService } from "../../core/services/storage.service";
@@ -13,17 +13,20 @@ export class WorkbookEffects {
     private storageService: StorageService
   ) {}
 
+  //TODO from - pipe
   initStorageAction$ = createEffect(() =>
     this.actions$.pipe(
       ofType(ROOT_EFFECTS_INIT),
       tap(x => console.log("init storage ngrx")),
       switchMap(() =>
-        this.storageService.initStorage().then(res => {
-          return initStorage();
-        })
+        from(this.storageService.initStorage())
+          .pipe(
+            map(x => initStorage())
+          )
       )),
   );
 
+  //TODO promise
   initWorkbookAction$ = createEffect(() =>
     this.actions$.pipe(
       ofType(initStorage),
